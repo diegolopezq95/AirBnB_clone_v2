b8912e617fe5b584e46591b83966ace219eb4d6f
diegolopezq95 <927@holbertonschool.com>
2019-12-17 19:01:25 +0000

    [Feature]: Task 10, fixing errors

--- models/place.py
+++ models/place.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 import os
+
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False), 
+                      Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
+                  )
 
 
 class Place(BaseModel, Base):
@@ -36,6 +42,7 @@ class Place(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", cascade="all, delete-orphan",
                                backref="place")
+        amenities = relationship("Amenity", secondary="place_amenity", viewonly=False, back_populates="place_amenities")
     else:
         @property
         def reviews(self):
@@ -46,3 +53,21 @@ class Place(BaseModel, Base):
                 if value["place_id"] == self.id:
                     my_list.append(value)
             return my_list
+
+        @property
+        def amenities(self): 
+            """returns the list of Amenity instances
+            """
+            my_list = []
+            for key_obj, value in models.storage.all(Amenity).items():
+                for amenit in self.amenity_ids: 
+                    if amenit == value.id:
+                        my_list.append(value)
+            return my_list
+                    
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter attribute amenities
+            """
+            if obj.__class__.__name__ == 'Amenity':
+                self.amenity_ids.append(obj.id)
