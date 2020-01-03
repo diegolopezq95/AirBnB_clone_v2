0cc9e80b05c82db3577c6c40c3f1205dad9cf289
diegolopezq95 <927@holbertonschool.com>
2020-01-03 19:22:55 +0000

    generates a .tgz archive from the contents of the web_static folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,11 +13,13 @@ def do_pack():
     """
     function that generates a .tgz archive
     """
-
-    local("mkdir -p versions")
-    date = datetime.now()
-    date_ = date.strftime("%Y%m%d%H%M%S")
-    tar_file = "tar -cvzf versions/web_static_{}.tgz ./web_static"\
-        .format(date_)
-    local(tar_file)
-    return ("/versions/web_static_{}.tgz")
+    try:
+        local("mkdir -p versions")
+        date = datetime.now()
+        date_ = date.strftime("%Y%m%d%H%M%S")
+        tar_file = "tar -cvzf versions/web_static_{}.tgz ./web_static"\
+            .format(date_)
+        local(tar_file)
+        return ("/versions/web_static_{}.tgz")
+    except:
+        return None
